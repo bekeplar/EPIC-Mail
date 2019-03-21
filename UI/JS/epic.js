be814902b2ead31a1446a107adaa480465e4c946
bekeplar <bekeplar@gmail.com>
2019-03-20 20:17:37 -0700

    Add challenge feedback and some code refactoring [chore-#164759682]

--- UI/JS/epic.js
+++ UI/JS/epic.js
@@ -1,13 +1,3 @@
- // Get the modal
- var modal = document.getElementById('id01');
-
- // When the user clicks anywhere outside of the modal, close it
- window.onclick = function(event) {
-     if (event.target == modal) {
-         modal.style.display = "none";
-     }
- }
-
  // Create a "close" button and append it to each list item
 var myNodelist = document.getElementsByTagName("LI");
 var i;
